bf3a4522680d239c341ac856b6d6f25e7eda54bd
Parker Hodge <iparkerhodge@gmail.com>
2023-06-20 14:28:17 -0500

    fix duplicate author bug

--- app/services/recipe_creator.rb
+++ app/services/recipe_creator.rb
@@ -4,6 +4,27 @@ class RecipeCreator < ApplicationService
               :total_time, :time_descriptions
 
   def initialize(**kwargs)
+    set_recipe(**kwargs)
+    set_author(**kwargs)
+
+    @categories = kwargs[:categories]
+  end
+
+  def call
+    ActiveRecord::Base.transaction do
+      @author.save! if @author.id.nil?
+      @author.recipes << @recipe
+
+      @categories.each do |name|
+        category = Category.find_or_create_by!(name: name)
+        @recipe.categories << category
+      end
+    end
+  end
+
+  private
+
+  def set_recipe(**kwargs)
     @recipe = Recipe.new
     @recipe.directions = kwargs[:directions]
     @recipe.image_url = kwargs[:image_url]
@@ -14,23 +35,14 @@ class RecipeCreator < ApplicationService
     @recipe.title = kwargs[:title]
     @recipe.total_time = kwargs[:total_time]
     @recipe.time_descriptions = kwargs[:time_descriptions]
+  end
+
+  def set_author(**kwargs)
+    @author = Author.find_by(name: kwargs[:author], profile: kwargs[:author_profile])
+    return if @author
 
     @author = Author.new
     @author.name = kwargs[:author]
     @author.profile = kwargs[:author_profile]
-
-    @categories = kwargs[:categories]
-  end
-
-  def call
-    ActiveRecord::Base.transaction do
-      @author.save!
-      @author.recipes << @recipe
-
-      @categories.each do |name|
-        category = Category.find_or_create_by!(name: name)
-        @recipe.categories << category
-      end
-    end
   end
 end
